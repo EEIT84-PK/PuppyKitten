0085ba415d9f818b5a6e4a61a242a6351061db5a
Zwen <kidd616227@gmail.com>
2016-04-06 11:00:05 +0800

    GoogleMap整合ver2.11

--- PuppyKitten/src/main/webapp/javascript/googlemap.js
+++ PuppyKitten/src/main/webapp/javascript/googlemap.js
@@ -3,11 +3,7 @@
 		var area;
 		var kind;
 		var select;
-<<<<<<< HEAD
-		
-=======
 		$('#datatable').DataTable();
->>>>>>> branch 'master' of https://github.com/EEIT84-PK/PuppyKitten.git
 		$('.text').hide().val('');
 		
 		$('.click').click(function(){
