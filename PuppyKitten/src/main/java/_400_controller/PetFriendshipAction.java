cd9b8f804a7adfa9e3bb94113f027f71245be198
ChaoiLin <b10057007@gmail.com>
2016-04-14 15:51:07 +0800

    確定聯誼系統沒任何bug

--- PuppyKitten/src/main/java/_400_controller/PetFriendshipAction.java
+++ PuppyKitten/src/main/java/_400_controller/PetFriendshipAction.java
@@ -1,5 +1,6 @@
 package _400_controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import com.opensymphony.xwork2.ActionSupport;
 
+import _400_model.PetAllBean;
 import _400_model.PetBean;
 import _400_model.PetImgBean;
 import _400_model.PetRelationBean;
@@ -17,7 +19,7 @@ import _400_model.PetSortDogBean;
 
 public class PetFriendshipAction extends ActionSupport implements ServletRequestAware {
 	private HttpServletRequest req;
-	
+
 	public HttpServletRequest getReq() {
 		return req;
 	}
@@ -31,125 +33,133 @@ public class PetFriendshipAction extends ActionSupport implements ServletRequest
 	}
 
 	public String execute() {
-		
+
 		PetService petService = new PetService();
-		List<PetBean> petBean = petService.selectAll();// 先將所有寵物資訊抓出來放到petBean內	
-		List<PetRelationBean>petRBean=petService.selectRelationAll();		
-		HttpSession session=req.getSession();
-		List<PetBean>selectKing=petService.selecPettId((Integer)session.getAttribute("memberID"));
-		System.out.println("selectKing="+selectKing.get(0).getPET_KING());
+		List<PetBean> petBean = petService.selectAll();// 先將所有寵物資訊抓出來放到petBean內
+		List<PetRelationBean> petRBean = petService.selectRelationAll();
+		HttpSession session = req.getSession();
+		List<PetBean> selectKing = petService.selecPettId((Integer) session.getAttribute("memberID"));
 		int number = 0;// 設定初始值為抓第一筆資料
-		int number2=number+1;//用來跟lise的size做比對
-		int check=0;
-		if (petBean.get(number).getPET_OWN_ID().equals(session.getAttribute("memberID"))) {			
-			if (number2==petBean.size()) {//因為如果list.get(index)==list.size 會有錯誤				
+		int number2 = number + 1;// 用來跟lise的size做比對
+		List<PetRelationBean> check = new ArrayList<PetRelationBean>();
+		List<PetRelationBean> check2 = new ArrayList<PetRelationBean>();
+		if (petBean.get(number).getPET_OWN_ID().toString().equals(session.getAttribute("memberID").toString())) {// 如果第一筆為自己
+			if (number2 == petBean.size()) { // 如果只有自己一個寵物資訊，代表沒有可感興趣的對象
 				session.setAttribute("end", "已經沒有可感興趣的對象");
 				return "end";
-			} else {				
-				number++;//這邊是指如果這次查的是自己 那要改成秀下一個編號的寵物
-				
-				for(int q=number;q<petBean.size();q++){
-					if(!petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())
-							||petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())							
-							&&petBean.get(number).getPET_SEX().equals(selectKing.get(0).getPET_SEX())
-							){
-						number++;
-					}else{
-						break;
-					}
-				}
-				
-				for(int i=0;i<petRBean.size();i++){
-					for(int j=number;j<petBean.size();j++){
-						if(petRBean.get(i).getINT_MENID_MYSELF().equals(session.getAttribute("memberID").toString())
-								&&petRBean.get(i).getINT_MENID_LIKE().equals(petBean.get(j).getPET_OWN_ID().toString())){							
-							number++;
-							break;
-						}						
-					}
-				}				
-				
-				if(number==petBean.size()){					
-					session.setAttribute("end", "已經沒有可感興趣的對象");
-					return "end";
-				}				
+			} else {
+				number++;// 跳下一筆
 			}
-		} else {
-			
-			for(int q=number;q<petBean.size();q++){
-				if(!petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())
-						||petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())							
-						&&petBean.get(number).getPET_SEX().equals(selectKing.get(0).getPET_SEX())
-						){
-					number++;
-				}else{
-					break;
-				}
-			}
-			
-			for(int i=0;i<petRBean.size();i++){
-				if(petRBean.get(i).getINT_MENID_MYSELF().equals(session.getAttribute("memberID").toString())){
-					for(int j=0;j<petBean.size();j++){					
-						if(petRBean.get(i).getINT_MENID_LIKE().equals(petBean.get(j).getPET_OWN_ID().toString())){							
-							number++;							
-							break;
-						}else {
-							if(session.getAttribute("memberID").toString().equals(petBean.get(j).getPET_OWN_ID().toString())){
-								if(check!=1){									
-									number++;
-									check++;									
-								}							
-							}
-						}					
-					}
-				}				
-			}				
-		}		
 
-		if(number==petBean.size()){
-			session.setAttribute("end", "已經沒有可感興趣的對象");
-			return "end";
-		}		
-		
-		//number有可能+2或3 如果喜歡的人連續+2 如果喜歡的人中間卡自己+3
-		if(petBean.get(number).getPET_OWN_ID().equals(session.getAttribute("memberID"))){
-			number++;			
-		}
-		
-		for(int q=number;q<petBean.size();q++){
-			if(!petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())
-					||petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())							
-					&&petBean.get(number).getPET_SEX().equals(selectKing.get(0).getPET_SEX())
-					){
-				number++;
-			}else{
-				break;
+			while (number < petBean.size()) {
+				if (petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())
+						&& (!(petBean.get(number).getPET_SEX().equals(selectKing.get(0).getPET_SEX())))) { // 如果是同類而且非同性的話
+					check = petService.selectId(session.getAttribute("memberID").toString(),
+							petBean.get(number).getPET_OWN_ID().toString(), "like");
+					check2 = petService.selectId(session.getAttribute("memberID").toString(),
+							petBean.get(number).getPET_OWN_ID().toString(), "Notlike");
+					/*
+					 * check=找喜歡人的人是自己 被喜歡的人是這次的人 狀態為喜歡 check2=找喜歡人的人是自己
+					 * 被喜歡的人是這次的人 狀態為不喜歡
+					 */
+					if (!check.isEmpty() || !check2.isEmpty()) {
+						number++;// 跳下一筆
+						check.clear();
+						check2.clear();
+						if (number == petBean.size()) {
+							session.setAttribute("end", "已經沒有可感興趣的對象");
+							return "end";
+						}
+					} else {
+						break;// 離開迴圈
+					}
+				} else if ((!(petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())))
+						|| (petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())
+								&& petBean.get(number).getPET_SEX().equals(selectKing.get(0).getPET_SEX()))) {// 如果是非同類或者同類同性的話
+					number++;// 跳下一筆
+					if (number == petBean.size()) {
+						session.setAttribute("end", "已經沒有可感興趣的對象");
+						return "end";
+					}
+				}
+			}
+		} else {// 如果第一筆不是自己
+			while (number < petBean.size()) {
+				if (petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())
+						&& (!(petBean.get(number).getPET_SEX().equals(selectKing.get(0).getPET_SEX())))) { // 如果是同類而且非同性的話
+					check = petService.selectId(session.getAttribute("memberID").toString(),
+							petBean.get(number).getPET_OWN_ID().toString(), "like");
+					check2 = petService.selectId(session.getAttribute("memberID").toString(),
+							petBean.get(number).getPET_OWN_ID().toString(), "Notlike");
+					/*
+					 * check=找喜歡人的人是自己 被喜歡的人是這次的人 狀態為喜歡 check2=找喜歡人的人是自己
+					 * 被喜歡的人是這次的人 狀態為不喜歡
+					 */
+					if ((!(check.isEmpty())) || (!(check2.isEmpty()))) {
+						number++;// 跳下一筆
+						check.clear();
+						check2.clear();
+
+						if (number == petBean.size()) {
+							session.setAttribute("end", "已經沒有可感興趣的對象");
+							return "end";
+						} else {
+							// 但是有可能下一筆是自己，所以再跳下一筆
+							if (petBean.get(number).getPET_OWN_ID().toString()
+									.equals(session.getAttribute("memberID").toString())) {
+								number++;
+							}
+						}
+
+						if (number == petBean.size()) {
+							session.setAttribute("end", "已經沒有可感興趣的對象");
+							return "end";
+						}
+					} else {
+						break;// 離開迴圈
+					}
+				} else if ((!(petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())))
+						|| (petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())
+								&& petBean.get(number).getPET_SEX().equals(selectKing.get(0).getPET_SEX()))) {// 如果是非同類或者同類同性的話
+					number++;// 跳下一筆
+
+					if (number == petBean.size()) {
+						session.setAttribute("end", "已經沒有可感興趣的對象");
+						return "end";
+					} else {
+						// 但是有可能下一筆是自己，所以再跳下一筆
+						if (petBean.get(number).getPET_OWN_ID().toString()
+								.equals(session.getAttribute("memberID").toString())) {
+							number++;
+						}
+					}
+
+					if (number == petBean.size()) {
+						session.setAttribute("end", "已經沒有可感興趣的對象");
+						return "end";
+					}
+				}
 			}
 		}
-		
-		if(number==petBean.size()){
-			session.setAttribute("end", "已經沒有可感興趣的對象");
-			return "end";
-		}		
-		
-		if (petBean.get(number).getPET_SORT_ID().startsWith("41")) {//如果抓出來的PET_SORT_ID是41開頭的話
-			//用此PET_SORT_ID去找對應的PET_SORT_NAME
-			PetSortCatBean Catbean = petService.selectSortCat(petBean.get(number).getPET_SORT_ID());			
+
+		if (petBean.get(number).getPET_SORT_ID().startsWith("41")) {// 如果抓出來的PET_SORT_ID是41開頭的話
+			// 用此PET_SORT_ID去找對應的PET_SORT_NAME
+			PetSortCatBean Catbean = petService.selectSortCat(petBean.get(number).getPET_SORT_ID());
 			session.setAttribute("Sortbean", Catbean);
 		} else {
-			PetSortDogBean Dogbean = petService.selectSortDog(petBean.get(number).getPET_SORT_ID());			
+			PetSortDogBean Dogbean = petService.selectSortDog(petBean.get(number).getPET_SORT_ID());
 			session.setAttribute("Sortbean", Dogbean);
 		}
 
-		Date now = new Date();//取得現在時間的所有秒數
-		//以下為算出現在時間-資料庫抓出來的出生日的時間再轉換毫秒(/1000)-->轉換天(/60*60*24)-->轉換年(/365)-->就是年齡了
-		long s = (now.getTime() - petBean.get(number).getPET_AGE().getTime()) / 1000 / (60 * 60 * 24) / 365;			
-		session.setAttribute("PET_AGE", s);				
+		Date now = new Date();// 取得現在時間的所有秒數
+		// 以下為算出現在時間-資料庫抓出來的出生日的時間再轉換毫秒(/1000)-->轉換天(/60*60*24)-->轉換年(/365)-->就是年齡了
+		long s = (now.getTime() - petBean.get(number).getPET_AGE().getTime()) / 1000 / (60 * 60 * 24) / 365;
+		session.setAttribute("PET_AGE", s);
 		session.setAttribute("petBean", petBean.get(number));
-		PetImgBean Imgbean = petService.selectId2(petBean.get(number).getPET_ID());				
-		session.setAttribute("petImg", Imgbean.getPET_IMAGE());				
-		session.setAttribute("PetNumber", ((Integer)number).toString());		
+		PetImgBean Imgbean = petService.selectId2(petBean.get(number).getPET_ID());
+		session.setAttribute("petImg", Imgbean.getPET_IMAGE());
+		session.setAttribute("PetNumber", ((Integer) number).toString());
 		return "success";
-		
+
 	}
 }
--- PuppyKitten/src/main/java/_400_controller/PetLikeAction.java
+++ PuppyKitten/src/main/java/_400_controller/PetLikeAction.java
@@ -1,5 +1,6 @@
 package _400_controller;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
@@ -33,114 +34,84 @@ public class PetLikeAction extends ActionSupport implements ServletRequestAware 
 
 	public String execute() {
 		PetService petService = new PetService();
-		HttpSession session=req.getSession();
-		String numberStr = (String) session.getAttribute("PetNumber");		
+		HttpSession session = req.getSession();
+		String numberStr = (String) session.getAttribute("PetNumber");
 		session.removeAttribute("PetNumber");// 為了避免之後要再傳值有衝突
-														// 所以把原本的number從session移除
+												// 所以把原本的number從session移除
 		int number = Integer.parseInt(numberStr); // 將String轉成Integer(此number為正在感興趣的對象編號)
-		List<PetBean> petBean = petService.selectAll();		
-				
+		List<PetBean> petBean = petService.selectAll();
 		PetRelationBean Rbean = new PetRelationBean();
 		Rbean.setINT_MENID_MYSELF(session.getAttribute("memberID").toString());// 登入者的會員編號
 		Rbean.setINT_MENID_LIKE(petBean.get(number).getPET_OWN_ID().toString());// 正在感興趣對象的主人編號
 		Rbean.setINT_STATUS("like");
 
 		PetRelationBean RbeanInsert = petService.insert(Rbean);
-		List<PetBean>selectKing=petService.selecPettId((Integer)session.getAttribute("memberID"));
-		if(number==0){
-			int checkSelf=number+1;//先確認下一筆是不是自己
-			if(petBean.get(checkSelf).getPET_OWN_ID().equals(session.getAttribute("memberID"))){
-				int checkNumber=checkSelf+1;
-				if(checkNumber==petBean.size()){
-					session.removeAttribute("end");
+		List<PetBean> selectKing = petService.selecPettId((Integer) session.getAttribute("memberID"));
+		List<PetRelationBean> check = new ArrayList<PetRelationBean>();
+		List<PetRelationBean> check2 = new ArrayList<PetRelationBean>();
+		number++;		
+		while (number < petBean.size()) {
+			if (petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())
+					&& !petBean.get(number).getPET_SEX().equals(selectKing.get(0).getPET_SEX())) { // 如果是同類而且非同性的話
+				check = petService.selectId(session.getAttribute("memberID").toString(),
+						petBean.get(number).getPET_OWN_ID().toString(), "like");
+				check2 = petService.selectId(session.getAttribute("memberID").toString(),
+						petBean.get(number).getPET_OWN_ID().toString(), "Notlike");
+				/*
+				 * check=找喜歡人的人是自己 被喜歡的人是這次的人 狀態為喜歡 check2=找喜歡人的人是自己 被喜歡的人是這次的人
+				 * 狀態為不喜歡
+				 */
+				if (!check.isEmpty() || !check2.isEmpty()) {
+					number++;// 跳下一筆
+					check.clear();
+					check2.clear();
+					if (number == petBean.size()) {
+						session.setAttribute("end", "已經沒有可感興趣的對象");
+						return "end";
+					}
+				} else {
+					break;// 離開迴圈
+				}
+			} else if ((!(petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())))
+					|| (petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())
+							&& petBean.get(number).getPET_SEX().equals(selectKing.get(0).getPET_SEX()))) {// 如果是非同類或者同類同性的話
+				number++;// 跳下一筆
+
+				if (number == petBean.size()) {
+					session.setAttribute("end", "已經沒有可感興趣的對象");
+					return "end";
+				} else {
+					// 但是有可能下一筆是自己，所以再跳下一筆
+					if (petBean.get(number).getPET_OWN_ID().toString()
+							.equals(session.getAttribute("memberID").toString())) {
+						number++;
+					}
+				}
+				if (number == petBean.size()) {
 					session.setAttribute("end", "已經沒有可感興趣的對象");
 					return "end";
 				}
-			}			
-		}				
-		
-		for(int q=number;q<petBean.size();q++){
-			if(!petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())
-					||petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())							
-					&&petBean.get(number).getPET_SEX().equals(selectKing.get(0).getPET_SEX())
-					){
-				number++;
-			}else{
-				break;
 			}
 		}
-		
-		int check2=number+1;
-		if(check2==petBean.size()){
-			session.removeAttribute("end");
+
+		if (number == petBean.size()) {
 			session.setAttribute("end", "已經沒有可感興趣的對象");
 			return "end";
 		}
-		
-		number++;// 下一個要讓使用者感興趣的對象編號
-		
-		for(int q=number;q<petBean.size();q++){
-			if(!petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())
-					||petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())							
-					&&petBean.get(number).getPET_SEX().equals(selectKing.get(0).getPET_SEX())
-					){
-				number++;
-			}else{
-				break;
-			}
-		}
-		
-		int check=number;
-		if(check==petBean.size()){
-			session.removeAttribute("end");
-			session.setAttribute("end", "已經沒有可感興趣的對象");
-			return "end";
-		}
-		
-		if(petBean.get(number).getPET_OWN_ID().equals(session.getAttribute("memberID"))){
-			number++;			
-			if(number==petBean.size()){
-				session.removeAttribute("end");
-				session.setAttribute("end", "已經沒有可感興趣的對象");
-				return "end";
-			}
-		}
-		
-		for(int q=number;q<petBean.size();q++){
-			if(!petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())
-					||petBean.get(number).getPET_KING().equals(selectKing.get(0).getPET_KING())							
-					&&petBean.get(number).getPET_SEX().equals(selectKing.get(0).getPET_SEX())
-					){
-				number++;
-			}else{
-				break;
-			}
-		}
-		
-		if(number==petBean.size()){
-			session.removeAttribute("end");
-			session.setAttribute("end", "已經沒有可感興趣的對象");
-			return "end";
-		}
-		
+
 		if (petBean.get(number).getPET_SORT_ID().startsWith("41")) {
 			PetSortCatBean Catbean = petService.selectSortCat(petBean.get(number).getPET_SORT_ID());
-			session.removeAttribute("Sortbean");
 			session.setAttribute("Sortbean", Catbean);
 		} else {
 			PetSortDogBean Dogbean = petService.selectSortDog(petBean.get(number).getPET_SORT_ID());
-			session.removeAttribute("Sortbean");
 			session.setAttribute("Sortbean", Dogbean);
 		}
 
 		Date now = new Date();
 		long s = (now.getTime() - petBean.get(number).getPET_AGE().getTime()) / 1000 / (60 * 60 * 24) / 365;
-		session.removeAttribute("PET_AGE");
 		session.setAttribute("PET_AGE", s);
-		session.removeAttribute("petBean");
 		session.setAttribute("petBean", petBean.get(number));
 		PetImgBean Imgbean = petService.selectId2(petBean.get(number).getPET_ID());
-		session.removeAttribute("petImg");
 		session.setAttribute("petImg", Imgbean.getPET_IMAGE());
 		session.setAttribute("PetNumber", ((Integer) number).toString());// 將此次number再丟進session給下一次用
 
