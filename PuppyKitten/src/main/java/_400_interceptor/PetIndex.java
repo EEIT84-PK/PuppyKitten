49a1330d3515b71485c05faf415a8836e3a3c916
Zwen <kidd616227@gmail.com>
2016-04-16 00:49:14 +0800

    commit

--- PuppyKitten/src/main/java/_400_interceptor/PetIndex.java
+++
@@ -1,9 +0,0 @@
-package _400_interceptor;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class PetIndex extends ActionSupport{
-	public String execute(){
-		return "success";
-	}
-}
--- PuppyKitten/src/main/java/_500_controller/ForgetPasswordAction.java
+++
@@ -1,8 +0,0 @@
-package _500_controller;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class ForgetPasswordAction extends ActionSupport{
-
-
-}
